cb259521240539855492aa7980b2799063aec571
Gabriel Augusto Ribeiro da Silva <gabrielsilva.webdeveloper@gmail.com>
2023-10-27 14:34:54 -0300

    commit inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <pre>
+      {{ quiz ? quiz : error }}
+    </pre>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      quiz: [],
+      error: null,  
+      category: null,
+      APIParams: {
+        'amount': null,
+        'category': null,
+        'difficulty': null,
+        'type': null
+      }
+    }
+  },
+  mounted() {
+    this.APIParams.amount = 10;
+    this.APIParams.category = 9;
+    this.APIParams.difficulty = 'medium';
+    this.APIParams.type = 'multiple';
+
+    axios
+      .get(`https://opentdb.com/api.php`, {
+        params: {
+          amount: this.APIParams.amount,
+          category: this.APIParams.category,
+          difficulty: this.APIParams.difficulty,
+          type: this.APIParams.type
+        }
+      })
+      .then(response => (this.quiz = response.data))
+      .catch(error => (this.error = error)); 
   }
 }
 </script>
